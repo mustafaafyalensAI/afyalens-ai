7c83907c3c9063e627c0a468f9497e3ffa2a0f79
mustafaafyalensAI <mustafapandu99@gmail.com>
2026-06-14 12:07:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,29 +2,21 @@ from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import os
 import requests
+import os
 
 app = Flask(__name__)
 
-MODEL_PATH = "malaria_model.keras"
-
 MODEL_URL = "https://drive.google.com/uc?export=download&id=1uiYBXrqtbm1X5F_bJEpOMrRs44_h0yYd"
+MODEL_PATH = "/tmp/malaria_model.keras"
 
-# Robust download function
 def download_model():
     if not os.path.exists(MODEL_PATH):
-        print("Downloading model...")
-
-        session = requests.Session()
-        response = session.get(MODEL_URL, stream=True)
-
+        r = requests.get(MODEL_URL, stream=True)
         with open(MODEL_PATH, "wb") as f:
-            for chunk in response.iter_content(1024):
+            for chunk in r.iter_content(1024):
                 if chunk:
                     f.write(chunk)
-
-        print("Model downloaded!")
 
 download_model()
 
@@ -36,30 +28,26 @@ def home():
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    try:
-        file = request.files["image"]
+    file = request.files["image"]
 
-        img = Image.open(file).convert("RGB")
-        img = img.resize((128,128))
-        img = np.array(img)/255.0
-        img = np.expand_dims(img, axis=0)
+    img = Image.open(file).convert("RGB")
+    img = img.resize((128,128))
+    img = np.array(img)/255.0
+    img = np.expand_dims(img, axis=0)
 
-        prediction = model.predict(img)[0][0]
+    prediction = model.predict(img)[0][0]
 
-        if prediction > 0.5:
-            result = "Uninfected"
-            confidence = float(prediction * 100)
-        else:
-            result = "Parasitized"
-            confidence = float((1 - prediction) * 100)
+    if prediction > 0.5:
+        result = "Uninfected"
+        confidence = float(prediction * 100)
+    else:
+        result = "Parasitized"
+        confidence = float((1 - prediction) * 100)
 
-        return jsonify({
-            "result": result,
-            "confidence": round(confidence, 2)
-        })
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    return jsonify({
+        "result": result,
+        "confidence": round(confidence, 2)
+    })
 
 
 if __name__ == "__main__":
