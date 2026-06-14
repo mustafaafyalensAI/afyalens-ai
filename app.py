0f97639866593a19ecd256b92d0762ac46b79c75
mustafaafyalensAI <mustafapandu99@gmail.com>
2026-06-14 11:31:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,33 @@ from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import os
+import requests
 
 app = Flask(__name__)
 
-model = tf.keras.models.load_model("malaria_model.keras")
+MODEL_PATH = "malaria_model.keras"
+
+MODEL_URL = "https://drive.google.com/uc?export=download&id=1uiYBXrqtbm1X5F_bJEpOMrRs44_h0yYd"
+
+# Robust download function
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print("Downloading model...")
+
+        session = requests.Session()
+        response = session.get(MODEL_URL, stream=True)
+
+        with open(MODEL_PATH, "wb") as f:
+            for chunk in response.iter_content(1024):
+                if chunk:
+                    f.write(chunk)
+
+        print("Model downloaded!")
+
+download_model()
+
+model = tf.keras.models.load_model(MODEL_PATH)
 
 @app.route("/")
 def home():
@@ -13,30 +36,31 @@ def home():
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
+    try:
+        file = request.files["image"]
 
-    if "image" not in request.files:
-        return jsonify({"error": "No image uploaded"})
+        img = Image.open(file).convert("RGB")
+        img = img.resize((128,128))
+        img = np.array(img)/255.0
+        img = np.expand_dims(img, axis=0)
 
-    file = request.files["image"]
+        prediction = model.predict(img)[0][0]
 
-    img = Image.open(file).convert("RGB")
-    img = img.resize((128,128))
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
+        if prediction > 0.5:
+            result = "Uninfected"
+            confidence = float(prediction * 100)
+        else:
+            result = "Parasitized"
+            confidence = float((1 - prediction) * 100)
 
-    prediction = model.predict(img)[0][0]
+        return jsonify({
+            "result": result,
+            "confidence": round(confidence, 2)
+        })
 
-    if prediction > 0.5:
-        result = "Uninfected"
-        confidence = float(prediction * 100)
-    else:
-        result = "Parasitized"
-        confidence = float((1 - prediction) * 100)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-    return jsonify({
-        "result": result,
-        "confidence": round(confidence, 2)
-    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
